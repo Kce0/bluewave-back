a7ae46f1086288f294497d2c6aef8bf416c28b51
Kce0 <ilovekce@naver.com>
2025-02-03 18:16:15 +0900

    수정2

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ app.use(cookieParser());
 dotenv.config();
 const port = process.env.PORT || 8000;
 
-const allowedOrigins = ["bluewave-a6e5f.web.app", "http://localhost:3000"];
+const allowedOrigins = [
+  "https://bluewave-a6e5f.web.app/",
+  "http://localhost:3000",
+];
 
 app.use(
   cors({
